bea331f02040446fa174c0f0c84c43f9f1708d68
zealisland <zealisland@gmail.com>
2021-04-17 09:08:41 -0700

    Update notify.py

--- notify.py
+++ notify.py
@@ -29,6 +29,7 @@ def check_availability(zip_codes, date_ranges, radius=25, vac_name=""):
                 match = re.search(r"Available Appointments:\s*(\d+)", text, re.MULTILINE)
                 if match is not None:
                     appointments = int(match.group(1))
+                    print("Debug: appointments = {appointments} \n")
                     if appointments >= 0:
                         text = '\n\t'.join([line for line in text.split('\n') if line.strip()])
                         print(f"Found Option: {urllib.parse.quote(url)} \n\t {text} \n\n")
